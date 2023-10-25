3391c41d0b683df522da221d08699cff1c4ee821
nnabilsayed <86965954+nnabilsayed@users.noreply.github.com>
2023-10-25 17:34:16 +0300

    first commit

---
+++ src/SearchForm.jsx
@@ -0,0 +1,27 @@
+import { useGlobalContext } from "./context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+    console.log(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button>search</button>
+      </form>
+    </section>
+  );
+};
+export default SearchForm;
---
+++ src/ThemeToggle.jsx
@@ -0,0 +1,13 @@
+import { useGlobalContext } from "./context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? <BsFillMoonFill /> : <BsFillSunFill />}
+      </button>
+    </section>
+  );
+};
+export default ThemeToggle;
